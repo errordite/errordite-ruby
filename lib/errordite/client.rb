9564473d59018e2cc9d2af7f3914ab552243ddab
Tom Ward <tom@popdog.net>
2013-04-17 22:51:45 +0100

    Write some rough specs for the client

--- lib/errordite/client.rb
+++ lib/errordite/client.rb
@@ -1,25 +1,26 @@
 require 'net/http'
 
 class Errordite::Client
-  attr_reader :key
+  attr_reader :server, :port
 
-  def initialize(key, server = 'www.errordite.com', port = 443)
-    @key = key
+  def initialize(server = 'www.errordite.com', port = 443)
     @server = server
     @port = port
   end
 
-  def send(message)
-    body = error.to_json
-    connection.post '/receiveerror', body, 'Content-Type' => 'application/json; charset=utf-8', 'Content-Length' => body.size.to_s
+  def send_json(path, body)
+    Connection.new(server, port).post path, body, 'Content-Type' => 'application/json; charset=utf-8', 'Content-Length' => body.size.to_s
   end
 
-  private
+  class Connection
+    def initialize(server, port)
+      @http = Net::HTTP.new server, port
+      @http.use_ssl = true
+      @http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
 
-  def connection
-    http = Net::HTTP.new @server, @port
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    http
+    def post(*args)
+      @http.post(*args)
+    end
   end
 end
--- spec/lib/errordite/client_spec.rb
+++ spec/lib/errordite/client_spec.rb
@@ -2,4 +2,33 @@ require 'spec_helper'
 require 'errordite/client'
 
 describe Errordite::Client do
+  let(:connection) { double(:connection) }
+
+  it 'sends json to the server by posting through a new connection' do
+    Errordite::Client::Connection.stub(:new).and_return(connection)
+    path = '/receiveerror'
+    body = '{}'
+    connection.should_receive(:post).with(
+      '/receiveerror', '{}', 'Content-Type' => 'application/json; charset=utf-8', 'Content-Length' => '2'
+    )
+    Errordite::Client.new.send_json(path, body)
+  end
+end
+
+describe Errordite::Client::Connection do
+  let(:http) { double(:http).as_null_object }
+
+  it 'creates a new verified https connection to the given server and port' do
+    Net::HTTP.stub(:new).with('one.example.com', 443).and_return(http)
+    http.should_receive(:use_ssl=).with(true)
+    http.should_receive(:verify_mode=).with(OpenSSL::SSL::VERIFY_PEER)
+    Errordite::Client::Connection.new('one.example.com', 443)
+  end
+
+  it 'posts data through the https connection' do
+    Net::HTTP.stub(:new).and_return(http)
+    arguments = double('arguments')
+    http.should_receive(:post).with(arguments)
+    Errordite::Client::Connection.new('one.example.com', 443).post(arguments)
+  end
 end
