6057888d22a90ec0d07ce26d6c5a7a3d50fe6709
Tom Ward <tom@popdog.net>
2013-04-17 22:52:56 +0100

    Simple convenience methods to record simple errors

--- lib/errordite.rb
+++ lib/errordite.rb
@@ -1,15 +1,35 @@
 require 'errordite/version'
 
 module Errordite
+  autoload :Client, 'errordite/client'
+  autoload :Rack, 'errordite/rack'
+  autoload :Serializer, 'errordite/serializer'
+
   class << self
     def api_token
-      @api_token
+      @api_token ||= ENV['ERRORDITE_TOKEN']
     end
 
     def api_token=(token)
       @api_token = token
     end
-  end
 
-  self.api_token = ENV['ERRORDITE_TOKEN']
+    def client
+      @client ||= Client.new
+    end
+
+    def client=(client)
+      @client = client
+    end
+
+    def record(error, context = {})
+      client.send_json '/receiveerror', Serializer.new(error, context).to_json
+    end
+
+    def monitor(context = {})
+      yield
+    rescue Exception => e
+      record e, context
+    end
+  end
 end
